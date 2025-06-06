68320c06cfd893112962e4e73fbf5b0198099e27
Orkun Alp Alim <106318587+OrkunAlpAlim@users.noreply.github.com>
2025-06-06 22:15:20 +0300

    Initial Commit

---
+++ myWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myWallet
+//
+//  Created by Orkun Alp Alim on 6.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ myWalletTests/myWalletTests.swift
@@ -0,0 +1,17 @@
+//
+//  myWalletTests.swift
+//  myWalletTests
+//
+//  Created by Orkun Alp Alim on 6.06.2025.
+//
+
+import Testing
+@testable import myWallet
+
+struct myWalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
