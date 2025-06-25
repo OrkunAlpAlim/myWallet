e189b718caf8e4bee32ac95ac35cd97fac147995
Orkun Alp Alim <106318587+OrkunAlpAlim@users.noreply.github.com>
2025-06-25 16:50:25 +0300

    fix ChatViewController issue

--- myWallet/ChatViewController.swift
+++ myWallet/ChatViewController.swift
@@ -6,7 +6,7 @@ class ChatViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     @IBOutlet weak var sendButton: UIButton!
-
+    
     var messages: [(text: String, from: String)] = []
     var currentUsername: String = ""
     var targetUsername: String = ""
@@ -14,15 +14,53 @@ class ChatViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-        view.addGestureRecognizer(tapGesture)
 
         self.title = targetFullName
         tableView.dataSource = self
         currentUsername = UserDefaults.standard.string(forKey: "currentUsername") ?? ""
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGesture)
+
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+
         listenMessages()
     }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    // MARK: - Keyboard Handling
+
+    @objc func keyboardWillShow(notification: Notification) {
+        if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            let keyboardHeight = keyboardFrame.height
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardHeight
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: Notification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
+    // MARK: - Firestore Chat
 
     func listenMessages() {
         let db = Firestore.firestore()
@@ -81,9 +119,7 @@ class ChatViewController: UIViewController, UITableViewDataSource {
                 }
             }
     }
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
+
     // MARK: - TableView
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
